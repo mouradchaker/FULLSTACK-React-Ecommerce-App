209ba727a9ec5db0433e145de670c7ce0389982f
mouradchaker <113807363+mouradchaker@users.noreply.github.com>
2023-03-09 23:32:22 +0100

    item-AddToCartFunc

--- client/src/components/Item.jsx
+++ client/src/components/Item.jsx
@@ -69,8 +69,26 @@ function Item({ item, width }) {
                 <AddIcon />
               </IconButton>
             </Box>
+
+            {/* BUTTON */}
+            <Button
+              onClick={() => dispatch(addToCart({ item: { ...item, count } }))}
+              sx={{ backgroundColor: shades.primary[300], color: "white" }}
+            >
+              Add to Cart
+            </Button>
           </Box>
         </Box>
+      </Box>
+
+      <Box mt="3px">
+        <Typography variant="subtitle2" color={neutral.dark}>
+          {category
+            .replace(/([A-Z])/g, "$1")
+            .replace(/^./, (str) => str.toUpperCase())}
+        </Typography>
+        <Typography>{name}</Typography>
+        <Typography fontWeight="bold">${price}</Typography>
       </Box>
     </Box>
   );
