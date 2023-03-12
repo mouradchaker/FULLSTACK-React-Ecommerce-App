659c8e058a350e95f5e4bb1761334fa698c436b9
mouradchaker <113807363+mouradchaker@users.noreply.github.com>
2023-03-13 00:54:02 +0100

    ShippingDone&AddressFormDone

--- client/src/scenes/Checkout/Shipping.jsx
+++ client/src/scenes/Checkout/Shipping.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Box, Checkbox, FormControlLabel, Typography } from "@mui/material";
+import AddressForm from "./AddressForm";
 
 const Shipping = ({
   values,
@@ -16,7 +17,50 @@ const Shipping = ({
         <Typography sx={{ mb: "15px" }} fontSize="18px">
           Billing Information
         </Typography>
+        <AddressForm
+          type="billingAddress"
+          values={values.billingAddress}
+          touched={touched}
+          errors={errors}
+          handleBlur={handleBlur}
+          handleChange={handleChange}
+        />
       </Box>
+
+      <Box mb="20px">
+        <FormControlLabel
+          label="Same for Shipping Address"
+          control={
+            <Checkbox
+              defaultChecked
+              value={values.shippingAddress.isSameAddress}
+              onChange={() =>
+                setFieldValue(
+                  "shippingAddress.isSameAddress",
+                  !values.shippingAddress.isSameAddress
+                )
+              }
+            />
+          }
+        />
+      </Box>
+
+      {/* SHIPPING FORM */}
+      {!values.shippingAddress.isSameAddress && (
+        <Box>
+          <Typography sx={{ mb: "15px" }} fontSize="18px">
+            Shipping Information
+          </Typography>
+          <AddressForm
+            type="shippingAddress"
+            values={values.shippingAddress}
+            touched={touched}
+            errors={errors}
+            handleBlur={handleBlur}
+            handleChange={handleChange}
+          />
+        </Box>
+      )}
     </Box>
   );
 };
